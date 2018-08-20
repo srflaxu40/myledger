8bba632025650c86d931ec6fa69ff472e0ec3daa
John Knepper <jknepper@Johns-MacBook-Pro.local>
2018-08-19 23:05:03 -0700

    goodnight

--- myledger-client/src/components/Login.js
+++ myledger-client/src/components/Login.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import '../css/base.css';
 
-class Nav extends Component {
+class Login extends Component {
 
   render() {
     return (
-       <div class="banner p-3 mb-2 bg-primary"></div> 
+       <div>
+       <div class="banner p-3 mb-2 bg-primary"></div>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="exampleEmail" hidden>Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+          </FormGroup>
+          {' '}
+          <FormGroup>
+            <Label for="examplePassword" hidden>Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+          </FormGroup>
+          {' '}
+          <Button>Submit</Button>
+        </form>
+       </div>
     );
   }
 }
 
-export default Nav;
+export default Login;
