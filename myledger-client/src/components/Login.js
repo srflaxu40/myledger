cfa8d81fa24f3062eda83ec74337edc70e615759
John Knepper <jknepper@Johns-MacBook-Pro.local>
2018-08-20 21:11:50 -0700

    Added welcome page

--- myledger-client/src/components/Login.js
+++ myledger-client/src/components/Login.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import '../css/base.css';
+import Welcome from './Welcome';
+import ReactDOM from 'react-dom';
+import { Route, Router } from 'react-router-dom';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(event) {
+    //const target = event.target
+    // this.setState({
+    //  [target.name]: target.value
+    //})
+    console.log("testicles");
+    this.props.push(`${this.state.where}`)
+    const Root = () => {
+
+      return (
+        <div className="base">
+          <Router history={Router}>
+            <Route path="/" component={Welcome}/>
+          </Router>
+        </div>
+      )
+    }
+
+    ReactDOM.render(<Root />, document.getElementById('root'));
+  }  
+
+  handleSubmit(event) {
+    event.preventDefault()
+    // do some sort of verification here if you need to
+    console.log("testicles");
+
+    ReactDOM.render(<Welcome />, document.getElementById('root'));
+  }
 
   render() {
     return (
        <div>
-       <div class="banner p-3 mb-2 bg-primary"></div>
+       <div className="banner p-3 mb-2 bg-primary"></div>
         <form onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label for="exampleEmail" hidden>Email</Label>
@@ -19,7 +57,13 @@ class Login extends Component {
             <Input type="password" name="password" id="examplePassword" placeholder="Password" />
           </FormGroup>
           {' '}
-          <Button>Submit</Button>
+          <FormGroup>
+            <input
+              type='submit'
+              name='submit'
+              className="button"
+            />
+          </FormGroup>
         </form>
        </div>
     );
---
+++ myledger-client/src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/base.css';
+
+class Welcome extends Component {
+
+  render() {
+    return (
+       <div>
+          tacos
+       </div>
+    );
+  }
+}
+
+export default Welcome;
